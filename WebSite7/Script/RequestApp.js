6221d5c95e5f44760d524fab5a7efe5464e607cb
cahartvig <cahartvig@gmail.com>
2017-04-20 02:03:41 +0200

    Add files via upload

---
+++ WebSite7/Script/RequestApp.js
@@ -0,0 +1,27 @@
+﻿var requestApp = angular.module('requestApp', []);
+
+requestApp.controller('requestController',
+    function($scope) {
+
+
+        $scope.submitForm = function (isValid) {
+            if (isValid) {
+                showSuccess();
+                hideForm();
+            }
+            if (!isValid) {
+                alert("Fejl: Din information er ikke valideret, eller sendt!");
+            }
+
+        };
+
+        function showSuccess() {
+            var ele = document.getElementById('success-message');
+            ele.style.display = 'block';
+        }
+        function hideForm() {
+            var ele2 = document.getElementById('main-form');
+            ele2.style.display = 'none';
+        }
+    });
+
